33bb1a637fdc5ca7551da84d8c17670f2ea1dfd5
Wenchao Hu <wenchaoh@andrew.cmu.edu>
2024-04-28 12:23:19 -0400

    git rid of theme selectors

--- static/js/index.js
+++ static/js/index.js
@@ -2,52 +2,9 @@ const root = document.querySelector('html');
 const body = document.querySelector('body');
 const mainNav = document.querySelector("nav");
 const menu = document.querySelector('.nav-links');
-const menuButton = document.getElementById('menu-display');
-const themeDisplay = document.getElementById('theme-display');
-const themeContainer = document.querySelector('.theme-container');
-const themeSelectors = document.getElementsByClassName('theme-select');
+const menuButton = document.getElementById('menu-display'); 
 
 mainNav.classList.add('js-nav');
-
-const getTheme = () => {
-  const theme = localStorage.getItem('theme');
-  theme && setActiveSelector(theme);
-  root.className = theme;
-  const shade = getComputedStyle(document.documentElement).getPropertyValue('--shade-100');
-  document.querySelector('meta[name="theme-color"]').setAttribute('content', shade);
-}
-
-const setTheme = (className) => {
-  var root = document.getElementsByTagName('html')[0];
-  root.className = className;
-  localStorage.setItem('theme', className);
-  const shade = getComputedStyle(document.documentElement).getPropertyValue('--shade-100');
-  document.querySelector('meta[name="theme-color"]').setAttribute('content', shade);
-  setActiveSelector(className);
-}
-
-const setActiveSelector = (className) => {
-  var selectedTheme = document.getElementById(`${className}-select`);
-  [...themeSelectors].forEach(item => {
-    item.classList.remove('active')
-  });
-  selectedTheme.classList.add('active');
-  hideThemeContainer();
-}
-
-const showThemeContainer = () => {
-  themeContainer.classList.add('visible');
-  [...themeSelectors].forEach(item => {
-    item.tabIndex = 0
-  });
-}
-
-const hideThemeContainer = () => {
-  themeContainer.classList.remove('visible');
-  [...themeSelectors].forEach(item => {
-    item.tabIndex = -1
-  });
-}
 
 const showMenu = () => {
   menu.classList.add('visible');
@@ -89,19 +46,7 @@ const handleNavScroll = () => {
   }
 }
 
-getTheme();
-
-themeDisplay.addEventListener("click", function () {
-  hideMenu()
-  if (themeContainer.classList.contains('visible')) {
-    hideThemeContainer();
-  } else {
-    showThemeContainer();
-  }
-})
-
-menuButton.addEventListener("click", function () {
-  hideThemeContainer();
+menuButton.addEventListener("click", function () { 
   if (menu.classList.contains('visible')) {
     hideMenu();
   } else {
@@ -109,8 +54,7 @@ menuButton.addEventListener("click", function () {
   }
 })
 
-menu.addEventListener("click", function () {
-  hideThemeContainer();
+menu.addEventListener("click", function () { 
   hideMenu()
 })
 
